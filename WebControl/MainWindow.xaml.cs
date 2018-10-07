2df84992b21f75216e94b3c61bd61bdb50b2c6d2
woodruff1606 <woodruff1606@gmail.com>
2018-10-06 20:58:43 -0500

    Commit

--- WebControl/MainWindow.xaml.cs
+++ WebControl/MainWindow.xaml.cs
@@ -16,7 +16,7 @@ using System.Windows.Shapes;
 namespace WebControl
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MainWindow.xaml 
     /// </summary>
     public partial class MainWindow : Window
     {
